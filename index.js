8b0857d847429bb5e2394d7e9b9bd00b444b3d81
Abel <acornejorojas20@gmail.com>
2023-10-23 01:05:27 -0500

    cambios en index.html

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ app.use(express.static(path.join(__dirname, 'public'));
 // Define una ruta para cargar tu calculadora
 app.get('/', (req, res) => {
   try {
-    // Tu código existente aquí
-    // Puede haber errores en esta sección
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
   } catch (error) {
-    console.error('Error inesperado:', error);
-    res.status(500).send('Ha ocurrido un error inesperado.');
-    // Puedes detener el servidor si es necesario
-    process.exit(1);
+    console.error('Error al cargar la calculadora:', error);
+    res.status(500).send('Error al cargar la calculadora. Por favor, inténtalo de nuevo más tarde.');
   }
+});
+
+// Agrega un manejador de errores global
+app.use((err, req, res, next) => {
+  console.error('Error no controlado:', err);
+  res.status(500).send('Ha ocurrido un error inesperado en la calculadora.');
 });
 
 // Puerto en el que se ejecutará el servidor

57cdb644c9b1c3b1b75aa5bb2fe7fe15f44643f8
Abel <acornejorojas20@gmail.com>
2023-10-23 20:16:14 -0500

    cambios

--- spec/support/miPruebaSpec.js
+++ spec/support/miPruebaSpec.js
@@ -21,6 +21,6 @@ describe("Mi prueba", function() {
 
   it("debería generar un error intencional", function() {
     // Agregar código que genere un error intencional
-    expect(true).toBe(true);
+    expect1234hola(true).toBe(true);
   });
 });
